590a9e6440255370ec392a5fed0a39b6e91ab6f6
Gonzalo Matheu <gonzalommj@gmail.com>
2014-09-26 18:20:28 -0300

    typo on docker container

--- docker/docker.sh
+++ docker/docker.sh
@@ -6,8 +6,8 @@ then
   alias docker-ip='sudo docker inspect --format "{{ .NetworkSettings.IPAddress }}" $1'
   alias docker-pid='sudo docker inspect --format "{{ .State.Pid }}" $1'
   alias docker-enter='sudo nsenter --target `docker-pid $1` -muinpw /bin/bash'
-  alias docker-bash='sudo docker run --rm -it ubuntu/14.04 /bin/bash'
-  alias docker-bash-share='sudo docker run -v `pwd`:/shared --rm -it ubuntu/14.04 /bin/bash'
+  alias docker-bash='sudo docker run --rm -it ubuntu:14.04 /bin/bash'
+  alias docker-bash-share='sudo docker run -v `pwd`:/shared --rm -it ubuntu:14.04 /bin/bash'
 fi
 
 if (which fig > /dev/null)
