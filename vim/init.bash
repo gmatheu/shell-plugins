291ba43181e2b99fe528262ee51ec4bd53662d69
Gonzalo Matheu <gonzalommj@gmail.com>
2014-12-02 10:22:54 -0200

    Adding vim plugin

---
+++ vim/init.bash
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+alias v=vim
+function install-vim-plugins(){
+  vim "+mkspell ~/.vim/spell/custom.en.utf-8.add" +PluginInstall +qall
+}
+function update-vim-plugins(){
+  vim "+mkspell ~/.vim/spell/custom.en.utf-8.add" +PluginUpdate +qall
+}
+
