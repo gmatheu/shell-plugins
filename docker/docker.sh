a41c1048fa4d5d15dda18d3a6413654b27e87c50
Gonzalo Matheu <gonzalommj@gmail.com>
2015-02-06 13:54:38 -0200

    Adding docker aliases

--- docker/docker.sh
+++ docker/docker.sh
@@ -2,12 +2,19 @@
 
 if (which docker > /dev/null)
 then
+  function docker-bash() {
+    sudo docker run --rm -it ubuntu:14.04 /bin/bash
+  }
+  function docker-bash-share(){
+    sudo docker run -v `pwd`:/shared --rm -it ubuntu:14.04 /bin/bash
+  }
+
   alias docker='sudo docker'
   alias docker-ip='sudo docker inspect --format "{{ .NetworkSettings.IPAddress }}" $1'
   alias docker-pid='sudo docker inspect --format "{{ .State.Pid }}" $1'
   alias docker-enter='sudo nsenter --target `docker-pid $1` -muinpw /bin/bash'
-  alias docker-bash='sudo docker run --rm -it ubuntu:14.04 /bin/bash'
-  alias docker-bash-share='sudo docker run -v `pwd`:/shared --rm -it ubuntu:14.04 /bin/bash'
+  alias dsh=docker-bash
+  alias dshs=docker-bash-share
 fi
 
 if (which fig > /dev/null)
