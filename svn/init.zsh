fbea61da88127fa822a3e22d82878ba5cfbd8502
Gonzalo Matheu <gonzalommj@gmail.com>
2015-02-24 22:17:59 -0300

    Adding svn alias

---
+++ svn/init.zsh
@@ -0,0 +1,12 @@
+#! /usr/bin/zsh
+
+alias sst='svn status'
+alias svc='svn commit'
+alias svu='svn update'
+alias svi='svn info'
+alias slo='svn log --limit 15'
+alias slov='svn log --limit 15 -v'
+alias slof='svn log --limit 15 --xml'
+alias sei='svn propedit svn:ignore'
+
+
