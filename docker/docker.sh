9bcba6b0d89f795e7a1b11a45025bd09e2299559
Gonzalo Matheu <gonzalommj@gmail.com>
2015-03-08 22:28:56 -0300

    Updating default docker image

--- docker/docker.sh
+++ docker/docker.sh
@@ -2,11 +2,12 @@
 
 if (which docker > /dev/null)
 then
+  local image=ubuntu:14.10
   function docker-bash() {
-    sudo docker run --rm -it ubuntu:14.04 /bin/bash
+    sudo docker run --rm -it $image /bin/bash
   }
   function docker-bash-share(){
-    sudo docker run -v `pwd`:/shared --rm -it ubuntu:14.04 /bin/bash
+    sudo docker run -v `pwd`:/shared --rm -it $image /bin/bash
   }
 
   alias docker='sudo docker'
