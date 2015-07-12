dba7bac092d4ba3cb43e55af821703ae2a1a20fb
Gonzalo Matheu <gonzalommj@gmail.com>
2015-07-12 18:48:37 -0300

    Adding glog with 30 entries limit

--- git/init.zsh
+++ git/init.zsh
@@ -2,11 +2,13 @@
 
 alias glo='git log --oneline --max-count=20 --reverse'
 compdef _git glo=git-log
-alias glof='git log --pretty=format:"%C(yellow)%h%Creset %s %Cred%an%Creset %Cgreen%ad" --max-count=15 --date=relative --reverse'
+alias glof='git log --pretty=format:"%C(yellow)%h%Creset %s %Cred%an%Creset %Cgreen%ad" --max-count=30 --date=relative --reverse'
 compdef _git glof=git-log
 alias gstl='git stash && git pull --rebase'
-alias gloc='git log --left-right --graph --cherry-pick --oneline'
+alias gloc='git log --left-right --graph --cherry-pick --oneline -n 30'
 compdef _git gloc=git-log
+alias glog='git log --oneline --decorate --color --graph -n 30'
+compdef _git glof=git-log
 alias gap='git add -p'
 compdef _git gap=git-add
 function _git_recent_branches() {
