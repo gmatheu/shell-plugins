cfc2266f708d30ea39f8a90203c5b0963d445c76
Gonzalo Matheu <gonzalommj@gmail.com>
2015-09-24 21:44:48 -0300

    Adding xclip

---
+++ xclip/init.sh
@@ -0,0 +1,8 @@
+#! /bin/sh
+
+alias xc='xclip -selection clipboard'
+alias xp='xclip'
+alias xs='xclip -selection secondary'
+alias xo='xclip -o'
+alias xoc='xclip -o -selection clipboard'
+alias xos='xclip -o -selection secondary'
