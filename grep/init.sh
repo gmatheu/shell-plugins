1faf8093b61c4125e5e8389eb0a611ce4d8a5d0c
Gonzalo Matheu <gonzalommj@gmail.com>
2015-09-24 21:44:18 -0300

    Adding grep

---
+++ grep/init.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+# Grep aliases
+function grep-aliases() {
+  alias | grep $1
+}
+
+# Grep rails routes
+function grep-rails-routes() {
+ 	rake routes | grep $1
+}
+
+function grep-recursive(){
+  grep -R -e $1 .
+}
+alias grr="grep-recursive"
+
+alias ga="grep-aliases"
+alias rrg="grep-rails-routes"
+alias rt="rake -T"
+alias psg="ps aux | grep $@"
+alias ack="ack-grep"
+
