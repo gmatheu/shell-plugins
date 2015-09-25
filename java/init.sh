a62c2f935b4e462e4c97ef361a74046602aa96a0
Gonzalo Matheu <gonzalommj@gmail.com>
2015-09-24 21:44:28 -0300

    Adding java

---
+++ java/init.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+alias use-java-6='export JAVA_HOME=/usr/lib/jvm/java-6-openjdk-amd64'
+alias use-java-7='export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64'
+alias use-java-7-oracle='export JAVA_HOME=/usr/lib/jvm/java-7-oracle'
+alias use-java-8-oracle='export JAVA_HOME=/usr/lib/jvm/java-8-oracle'
