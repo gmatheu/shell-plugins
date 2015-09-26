144104e442cb8b420f327e8e62cdbe78dc1056b8
Gonzalo Matheu <gonzalommj@gmail.com>
2015-09-26 20:02:28 -0300

    Fixing shellcheck issues

--- docker/docker.sh
+++ docker/docker.sh
@@ -7,15 +7,18 @@ then
     sudo docker run --rm -it $image /bin/bash
   }
   function docker-bash-share(){
-    sudo docker run -v `pwd`:/shared --rm -it $image /bin/bash
+    sudo docker run -v "$(pwd)":/shared --rm -it $image /bin/bash
+  }
+  function docker-ip() {
+    sudo docker inspect --format "{{ .NetworkSettings.IPAddress }}" "$1"
+  }
+  function docker-pid() {
+    sudo docker inspect --format "{{ .State.Pid }}" "$1"
   }
 
   alias docker='sudo docker'
-  alias docker-ip='sudo docker inspect --format "{{ .NetworkSettings.IPAddress }}" $1'
   alias dip=docker-ip
-  alias docker-pid='sudo docker inspect --format "{{ .State.Pid }}" $1'
   alias dpid=docker-pid
-  alias docker-enter='sudo nsenter --target `docker-pid $1` -muinpw /bin/bash'
   alias dsh=docker-bash
   alias dshs=docker-bash-share
 fi
