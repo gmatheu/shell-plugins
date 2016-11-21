146c98ee55ad5b6c660449918ddec77cf96b3efc
Gonzalo Matheu <gonzalommj@gmail.com>
2016-11-21 09:25:09 -0300

    Removing sudo calls on docker plugin

--- docker/docker.sh
+++ docker/docker.sh
@@ -4,19 +4,18 @@ if (which docker > /dev/null)
 then
   local image=ubuntu:14.10
   function docker-bash() {
-    sudo docker run --rm -it $image /bin/bash
+    docker run --rm -it $image /bin/bash
   }
   function docker-bash-share(){
-    sudo docker run -v "$(pwd)":/shared --rm -it $image /bin/bash
+    docker run -v "$(pwd)":/shared --rm -it $image /bin/bash
   }
   function docker-ip() {
-    sudo docker inspect --format "{{ .NetworkSettings.IPAddress }}" "$1"
+    docker inspect --format "{{ .NetworkSettings.IPAddress }}" "$1"
   }
   function docker-pid() {
-    sudo docker inspect --format "{{ .State.Pid }}" "$1"
+    docker inspect --format "{{ .State.Pid }}" "$1"
   }
 
-  alias docker='sudo docker'
   alias dip=docker-ip
   alias dpid=docker-pid
   alias dsh=docker-bash
@@ -25,6 +24,5 @@ fi
 
 if (which docker-compose > /dev/null)
 then
-  alias docker-compose='sudo docker-compose'
   alias dcp=docker-compose
 fi
