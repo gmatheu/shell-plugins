f308812830fc20d76c54f3f13b8aba6eaa6e5a45
Gonzalo Matheu <gmatheu@ascentio.com.ar>
2018-03-29 17:42:57 -0300

    Switching to vim Plug

--- vim/init.bash
+++ vim/init.bash
@@ -2,10 +2,10 @@
 
 alias v=vim
 function install-vim-plugins(){
-  vim "+mkspell ~/.vim/spell/custom.en.utf-8.add" +PluginInstall +qall
+  vim "+mkspell ~/.vim/spell/custom.en.utf-8.add" +PlugInstall +qall
 }
 function update-vim-plugins(){
-  vim "+mkspell ~/.vim/spell/custom.en.utf-8.add" +PluginUpdate +qall
+  vim "+mkspell ~/.vim/spell/custom.en.utf-8.add" +PlugUpdate +qall
 }
 function edit-vim-plugins(){
   vim ~/.vim/plugins.txt
