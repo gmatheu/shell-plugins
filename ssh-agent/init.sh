87f0b5c00dfef43c2b0cbf8d96d6f6c38129c12a
Gonzalo Matheu (framework) <gmatheu@simtlix.com>
2022-10-11 12:57:56 -0300

    ssh-agent: Adding docsstring and aliases

--- ssh-agent/init.sh
+++ ssh-agent/init.sh
@@ -1,19 +1,40 @@
 #! /bin/sh
+## # ssh-agent plugin
+##
+## Provides several functions and aliases to manage a single instance of a ssh-agent across
+## multiple terminal instances.
+##
+## Requires: fzf and/or gum to provide fuzzy search
 
 
 SSH_AGENT_MUTEX=~/.current_ssh_agent
 SSH_HOME=$HOME/.ssh
 
-ssh-agent-create () {
-  ssh-agent > $SSH_AGENT_MUTEX
-}
 ssh-agent-activate-current () {
+  ### Activates the current ssh agent.
+  echo "Activating current ssh-agent: $SSH_AGENT_PID"
   source $SSH_AGENT_MUTEX
 }
+
+ssh-agent-create () {
+  ### Creates a new ssh-agent and store its environment variables into $SSH_AGENT_MUTEX
+  ssh-agent > $SSH_AGENT_MUTEX
+  ssh-agent-activate-current
+  echo "Current ssh-agent created: $SSH_AGENT_PID"
+}
+
+ssh-agent-kill-current () {
+  ### Kills current ssh agent.
+  echo "Killing current ssh agent: $SSH_AGENT_PID"
+  kill -9 $SSH_AGENT_PID
+  unset $SSH_AGENT_PID
+}
+
 ssh-agent-list-identities () {
   echo "> Loaded Entities"
   ssh-add -l
 }
+
 ssh-agent-add-identities () {
   ssh-agent-list-identities
   echo "------------"
@@ -25,3 +46,9 @@ ssh-agent-add-identity () {
   echo "------------"
   find $SSH_HOME/ -type f -not -name '*.pub' -not -name 'known_*' -printf '%f\n' | fzf | xargs -o -I {} ssh-add $SSH_HOME/{}
 }
+
+alias sac=ssh-agent-activate-current
+alias sal=ssh-agent-list-identities
+alias saa=ssh-agent-add-identity
+alias sas=ssh-agent-add-identities
+alias sad=ssh-agent-remove-identity
