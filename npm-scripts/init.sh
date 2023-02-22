a27c4f984fb57741f6c07f383037d9cf840271d2
Gonzalo Matheu (framework) <gmatheu@simtlix.com>
2023-02-22 12:57:06 -0300

    npm-scripts: Initializing npm-scripts plugin

--- npm-scripts/init.sh
+++ npm-scripts/init.sh
@@ -1,19 +1,21 @@
 #! /bin/sh
 #
 function _npm-scripts-inline { 
-  npx json -f package.json scripts | npx json -e 'this.lines = Object.keys(this).map(l => { return `${l}|${this[l]}`})' lines | json -a 
+  npx json -f package.json scripts |\
+    npx json -e 'this.lines = Object.keys(this).map(l => { return `${l}|${this[l]}`})' lines |\
+    json -a 
 }
 
 function npm-run-scripts-find { 
-  echo $(_npm-scripts-inline | fzf | cut -d '|' -f 1 )
+  echo $(_npm-scripts-inline | fzf) | cut -d '|' -f 1
 }
 function npm-run-scripts { 
   script=$(npm-run-scripts-find)
   echo "Executing -> ${script}"
-  npx run ${script}
+  npm run ${script}
 }
 function npm-run-scripts-show { 
-  echo $(_npm-scripts-inline | fzf | cut -d '|' -f 2)
+echo $(_npm-scripts-inline | fzf) | cut -d '|' -f 2
 }
 
 alias npms=npm-run-scripts
