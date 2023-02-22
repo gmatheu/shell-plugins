e7b7e02cf5a26988f1341dcfacf68c1b0be14f2e
Gonzalo Matheu (framework) <gmatheu@simtlix.com>
2023-02-22 12:10:59 -0300

    npm-scripts: Initializing npm-scripts plugin

---
+++ npm-scripts/README.md
@@ -0,0 +1,30 @@
+
+ # npm scripts plugin
+
+Helper functions to fastly search and execute npm scripts
+
+ Requires: fzf to provide fuzzy search
+
+
+## Functions
+
+### npm-run-scripts
+
+Find script and execute selected script
+
+
+### npm-run-scripts-find
+
+Find and show script name
+
+
+### npm-run-scripts-show
+
+Find script and show its content
+
+
+## Aliases
+
+* *npms*: npm-run-scripts
+* *npmss*: npm-run-scripts-show
+* *npmsf*: npm-run-scripts-find
---
+++ npm-scripts/init.sh
@@ -0,0 +1,17 @@
+#! /bin/sh
+#
+function _npm-scripts-inline { 
+  npx json -f package.json scripts | npx json -e 'this.lines = Object.keys(this).map(l => { return `${l}|${this[l]}`})' lines | json -a 
+}
+
+function npm-run-scripts-find { 
+  echo $(_npm-scripts-inline | fzf | cut -d '|' -f 1 )
+}
+function npm-run-scripts { 
+  script=$(_npm-run-scripts-find)
+  echo "Executing -> ${script}"
+  npx run ${script}
+}
+function npm-run-scripts-show { 
+  echo $(_npm-scripts-inline | fzf | cut -d '|' -f 2)
+}
