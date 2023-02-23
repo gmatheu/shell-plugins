e8cbaffba2941b47bc703331d5edd14f9d34386e
Gonzalo Matheu (framework) <gmatheu@simtlix.com>
2023-02-22 23:22:56 -0300

    :zplug-helper: Adding debug mode and properly filtering skipped directories [oh-my-zsh, spaceship by default]

--- zplug-helper/init.sh
+++ zplug-helper/init.sh
@@ -10,8 +10,22 @@ to_skip=("oh-my-zsh" "spaceship")
 
 zplug-plugin-help () {
   ### Looks for all plugins with README.md files and shows the selected one content
-  help_dir=$(find ${BASE_DIR} -mindepth 4 -name 'README.md' | xargs -I {} dirname {} | sort | uniq | fzf)
+  findargs=()
+  for i in "${to_skip[@]}"; do
+       findargs+=('-not' '-path' '*'"$i*"'*')
+  done
+  find_cmd=$(echo find ${BASE_DIR} -mindepth 4 -name 'README.md' -and "${findargs[@]}" )
+  if [ ! -z "${DEBUG}" ]
+  then
+    echo "Find command ->" ${find_cmd}
+  fi
+  help_dir=$(find ${BASE_DIR} -mindepth 4 -name 'README.md' -and "${findargs[@]}" | xargs -I {} dirname {} | sort | uniq | fzf)
   cat ${help_dir}/README.md | gum format
 }
 
 alias zh=zplug-plugin-help
+
+if [ ! -z "${DEBUG}" ]
+then
+  zplug-plugin-help
+fi
