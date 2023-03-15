6e504a4d151eb16fd72db4e3cca56ee82873654b
Gonzalo Matheu (framework) <gmatheu@simtlix.com>
2023-03-15 12:21:32 -0300

    fzf: Adding grep-fzf for content search

--- fzf/README.md
+++ fzf/README.md
@@ -2,7 +2,9 @@
 
  Helper functions to work with fzf
 
- Instals fzf if is not available
+ Installs fzf if is not available
+ 
+ Requires: rg (ripgrep) 
 
 
 ## Functions
@@ -16,9 +18,13 @@
 ### edit-fzf 
    Shows fzf and opens selected file with default editor
 
+### grep-fzf 
+   Search content, fuzzy search and opens selected file with default editor
+
 
 ## Aliases
 
 * *ez*: edit-fzf
+* *eg*: grep-fzf
 * *launcher*: cli-launcher
 * *cl*: cli-launcher
--- fzf/init.sh
+++ fzf/init.sh
@@ -3,7 +3,9 @@
 ##
 ## Helper functions to work with fzf
 ##
-## Instals fzf if is not available
+## Installs fzf if is not available
+## 
+## Requires: rg (ripgrep) 
 
 fzf () {
   ### Lazy loads fzf and installs it if not present
@@ -13,7 +15,9 @@ fzf () {
     && git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf \
     && $HOME/.fzf/install --no-update-rc
   [ -f ~/.fzf.zsh ] \
-    && source ~/.fzf.zsh
+    && source ~/.fzf.zsh \
+    && [[ $- == *i* ]] && source "/home/gmatheu/.fzf/shell/completion.zsh" 2> /dev/null \
+    && source "/home/gmatheu/.fzf/shell/key-bindings.zsh"
   fzf
 }
 cli-launcher () {
@@ -30,7 +34,15 @@ edit-fzf () {
   print -s "$EDITOR ${selected}"
   $EDITOR ${selected}
 }
+grep-fzf () {
+  ### Search content, fuzzy search and opens selected file with default editor
+  selected=$(rg -L --color=never --no-heading --with-filename --line-number --column --smart-case . | fzf)
+  filename=$(echo $selected | cut -d : -f 1)
+  print -s "$EDITOR ${filename}"
+  $EDITOR ${filename}
+}
 
 alias ez=edit-fzf
+alias eg=grep-fzf
 alias launcher=cli-launcher
 alias cl=cli-launcher
