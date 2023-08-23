d8770afe55476a92167322f1a4584c4cc8afef46
Gonzalo Matheu (framework) <gmatheu@simtlix.com>
2023-08-23 10:08:56 -0300

    clipboard: Adding xp (xclip_full_path) to copy absolute file paths

---
+++ clipboard/README.md
@@ -0,0 +1,22 @@
+# # clipboard plugin
+
+ Functions to manage clipboard
+
+ Requires: 
+    * xclip 
+
+
+## Functions
+
+### xclip_full_path
+   Shows and copies to clipboard full path of given file
+
+
+## Aliases
+
+* *xc*: 'xclip -selection clipboard'
+* *xp*: xclip_full_path
+* *xs*: 'xclip -selection secondary'
+* *xo*: 'xclip -o'
+* *xoc*: 'xclip -o -selection clipboard'
+* *xos*: 'xclip -o -selection secondary'
--- clipboard/init.sh
+++ clipboard/init.sh
@@ -1,12 +1,27 @@
 #! /bin/sh
+### # clipboard plugin
+##
+## Functions to manage clipboard
+##
+## Requires: 
+##    * xclip 
 
 command -v xclip > /dev/null 2>&1 && {
-  alias xc='xclip -selection clipboard'
-  alias xp='xclip'
-  alias xs='xclip -selection secondary'
-  alias xo='xclip -o'
-  alias xoc='xclip -o -selection clipboard'
-  alias xos='xclip -o -selection secondary'
+
+xclip_full_path() {
+  ### Shows and copies to clipboard full path of given file
+  _filename=$1
+  _path=$(realpath "${_filename}")
+  echo "${_path}"
+  echo "${_path}" | xclip -selection clipboard
+}
+
+alias xc='xclip -selection clipboard'
+alias xp=xclip_full_path
+alias xs='xclip -selection secondary'
+alias xo='xclip -o'
+alias xoc='xclip -o -selection clipboard'
+alias xos='xclip -o -selection secondary'
 }
 
 command -v pbcopy > /dev/null 2>&1 && {
