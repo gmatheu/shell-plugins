8401c266245e2efde328e95319bcf8b275e4e315
Gonzalo Matheu (framework) <gonzalommj@gmail.com>
2024-11-19 08:52:52 -0300

    ssh-agent: Listing identities when loading ssh-agent

--- ssh-agent/init.sh
+++ ssh-agent/init.sh
@@ -6,34 +6,39 @@
 ##
 ## Requires: fzf and/or gum to provide fuzzy search
 
-
 SSH_AGENT_DEFAULT_ALIAS='default'
 SSH_AGENT_MUTEX_HOME=${HOME}
 SSH_AGENT_MUTEX=${SSH_AGENT_MUTEX_HOME}/.current_ssh_agent
 SSH_HOME=$HOME/.ssh
 
-ssh_agent_activate_current () {
+ssh_agent_list_identities() {
+  echo "> Loaded Identities"
+  ssh-add -l
+}
+
+ssh_agent_activate_current() {
   ### Activates the current ssh agent.
   alias=${1:-${SSH_AGENT_DEFAULT_ALIAS}}
   echo "Activating ${alias} ssh-agent: $SSH_AGENT_PID"
   source $SSH_AGENT_MUTEX.${alias}
+
+  ssh_agent_list_identities
 }
 
-ssh_agent_list () {
+ssh_agent_list() {
   ### List existing agents
   find "${SSH_AGENT_MUTEX_HOME}" -maxdepth 1 -name '.current_ssh_agent.*' | cut -d '.' -f 3
 }
 
-
-ssh_agent_create () {
+ssh_agent_create() {
   ### Creates a new ssh-agent and store its environment variables into $SSH_AGENT_MUTEX
   alias=${1:-${SSH_AGENT_DEFAULT_ALIAS}}
-  ssh-agent > "$SSH_AGENT_MUTEX.${alias}"
+  ssh-agent >"$SSH_AGENT_MUTEX.${alias}"
   ssh_agent_activate_current "${alias}"
   echo "${alias} ssh-agent created: $SSH_AGENT_PID"
 }
 
-ssh_agent_kill_current () {
+ssh_agent_kill_current() {
   ### Kills current ssh agent.
   echo "Killing current ssh agent: $SSH_AGENT_PID"
   kill -9 "$SSH_AGENT_PID"
@@ -41,18 +46,13 @@ ssh_agent_kill_current () {
   unset "$SSH_AGENT_PID"
 }
 
-ssh_agent_list_identities () {
-  echo "> Loaded Entities"
-  ssh-add -l
-}
-
-ssh_agent_add_identities () {
+ssh_agent_add_identities() {
   ssh_agent_list_identities
   echo "------------"
   find "$SSH_HOME" -type f -not -name '*.pub' -not -name 'known_*' -printf '%f\n' | gum choose --no-limit | xargs -o -I {} ssh-add "$SSH_HOME"/{}
 }
 
-ssh_agent_add_identity () {
+ssh_agent_add_identity() {
   ssh_agent_list_identities
   echo "------------"
   find "$SSH_HOME"/ -type f -not -name '*.pub' -not -name 'known_*' -printf '%f\n' | fzf | xargs -o -I {} ssh-add "$SSH_HOME"/{}
