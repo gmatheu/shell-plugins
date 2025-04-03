17daea79ca021f57cdaec9d8b1625e008206e868
Gonzalo Matheu (framework) <gonzalommj@gmail.com>
2025-04-03 17:05:51 -0300

    ssh-agent: Storing loaded identities to be able to resurrect

--- ssh-agent/init.sh
+++ ssh-agent/init.sh
@@ -9,6 +9,8 @@
 SSH_AGENT_DEFAULT_ALIAS='default'
 SSH_AGENT_MUTEX_HOME=${HOME}
 SSH_AGENT_MUTEX=${SSH_AGENT_MUTEX_HOME}/.current_ssh_agent
+SSH_AGENT_IDENTITIES=${SSH_AGENT_MUTEX_HOME}/.current_ssh_agent_identities
+SSH_AGENT_ACTIVE=${SSH_AGENT_MUTEX_HOME}/.active_ssh_agent
 SSH_HOME=$HOME/.ssh
 
 ssh_agent_list_identities() {
@@ -16,11 +18,34 @@ ssh_agent_list_identities() {
   ssh-add -l
 }
 
+_ssh_agent_save_identities() {
+  identity=$1
+  local identities_file
+  identities_file="$SSH_AGENT_IDENTITIES.$(cat "${SSH_AGENT_ACTIVE}")"
+  echo "$identity" >>"$identities_file"
+  local content
+  content=$(cat ${identities_file} | sort | uniq)
+  echo "$content" >"$identities_file"
+}
+
+_ssh_agent_clear_identities() {
+  identities_file="$SSH_AGENT_IDENTITIES.$(cat "${SSH_AGENT_ACTIVE}")"
+  truncate -f -s 0 "$identities_file"
+}
+_ssh_agent_restore_identities() {
+  identities_file="$SSH_AGENT_IDENTITIES.$(cat "${SSH_AGENT_ACTIVE}")"
+  echo "Restoring identities"
+  for identity in $(cat "$identities_file"); do
+    ssh-add "$identity"
+  done
+}
+
 ssh_agent_activate_current() {
   ### Activates the current ssh agent.
   alias=${1:-${SSH_AGENT_DEFAULT_ALIAS}}
   echo "Activating ${alias} ssh-agent: $SSH_AGENT_PID"
   source $SSH_AGENT_MUTEX.${alias}
+  echo "${alias}" >"$SSH_AGENT_ACTIVE"
 
   ssh-add -l >/dev/null 2>&1
   ret=$?
@@ -60,17 +85,25 @@ ssh_agent_kill_current() {
 ssh_agent_add_identities() {
   ssh_agent_list_identities
   echo "------------"
-  find "$SSH_HOME" -type f -not -name '*.pub' -not -name 'known_*' -printf '%f\n' | gum choose --no-limit | xargs -o -I {} ssh-add "$SSH_HOME"/{}
+  for identity in $(find "$SSH_HOME" -type f -not -name '*.pub' -not -name 'known_*' -printf '%f\n' | gum choose --no-limit); do
+    ssh-add "$SSH_HOME/$identity"
+    _ssh_agent_save_identities "$SSH_HOME/$identity"
+  done
 }
 
 ssh_agent_add_identity() {
   ssh_agent_list_identities
   echo "------------"
-  find "$SSH_HOME"/ -type f -not -name '*.pub' -not -name 'known_*' -printf '%f\n' | fzf | xargs -o -I {} ssh-add "$SSH_HOME"/{}
+  for identity in $(find "$SSH_HOME"/ -type f -not -name '*.pub' -not -name 'known_*' -printf '%f\n' | fzf); do
+    ssh-add "$SSH_HOME/$identity"
+    _ssh_agent_save_identities "$SSH_HOME/$identity"
+  done
 }
 
 alias ssh-agent-create=ssh_agent_create
 alias sac=ssh_agent_activate_current
+alias sacr=_ssh_agent_restore_identities
+alias sacc=_ssh_agent_clear_identities
 alias saci=ssh_agent_activate_interactive
 alias sal=ssh_agent_list_identities
 alias saa=ssh_agent_add_identity
